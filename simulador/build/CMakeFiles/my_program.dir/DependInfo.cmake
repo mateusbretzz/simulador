fbd99a447f1464c9d1be5aef22538211331f12a9
mateuss <teubr2202@gmail.com>
2024-06-04 18:08:34 -0300

    Adicionando cMake

---
+++ simulador/build/CMakeFiles/my_program.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/Amuleto.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/Amuleto.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/Amuleto.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/Capa.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/Capa.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/Capa.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/Colete.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/Colete.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/Colete.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/Colher.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/Colher.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/Colher.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/Escudo.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/Escudo.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/Escudo.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/Flecha.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/Flecha.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/Flecha.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/Garfo.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/Garfo.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/Garfo.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/Katana.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/Katana.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/Katana.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/Livro.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/Livro.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/Livro.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/Machado.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/Machado.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/Machado.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/Martelo.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/Martelo.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/Martelo.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/ProtetorSolar.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/ProtetorSolar.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/ProtetorSolar.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/acessorios-cpp/Rosa.cpp" "CMakeFiles/my_program.dir/acessorios-cpp/Rosa.cpp.o" "gcc" "CMakeFiles/my_program.dir/acessorios-cpp/Rosa.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/core-simulador-cpp/ArmaAtaque.cpp" "CMakeFiles/my_program.dir/core-simulador-cpp/ArmaAtaque.cpp.o" "gcc" "CMakeFiles/my_program.dir/core-simulador-cpp/ArmaAtaque.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/core-simulador-cpp/ArmaDefesa.cpp" "CMakeFiles/my_program.dir/core-simulador-cpp/ArmaDefesa.cpp.o" "gcc" "CMakeFiles/my_program.dir/core-simulador-cpp/ArmaDefesa.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/core-simulador-cpp/Personagem.cpp" "CMakeFiles/my_program.dir/core-simulador-cpp/Personagem.cpp.o" "gcc" "CMakeFiles/my_program.dir/core-simulador-cpp/Personagem.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/core-simulador-cpp/Simulador.cpp" "CMakeFiles/my_program.dir/core-simulador-cpp/Simulador.cpp.o" "gcc" "CMakeFiles/my_program.dir/core-simulador-cpp/Simulador.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/personagens-cpp/Chaves.cpp" "CMakeFiles/my_program.dir/personagens-cpp/Chaves.cpp.o" "gcc" "CMakeFiles/my_program.dir/personagens-cpp/Chaves.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/personagens-cpp/DarthVader.cpp" "CMakeFiles/my_program.dir/personagens-cpp/DarthVader.cpp.o" "gcc" "CMakeFiles/my_program.dir/personagens-cpp/DarthVader.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/personagens-cpp/Elsa.cpp" "CMakeFiles/my_program.dir/personagens-cpp/Elsa.cpp.o" "gcc" "CMakeFiles/my_program.dir/personagens-cpp/Elsa.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/personagens-cpp/Groot.cpp" "CMakeFiles/my_program.dir/personagens-cpp/Groot.cpp.o" "gcc" "CMakeFiles/my_program.dir/personagens-cpp/Groot.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/personagens-cpp/Simba.cpp" "CMakeFiles/my_program.dir/personagens-cpp/Simba.cpp.o" "gcc" "CMakeFiles/my_program.dir/personagens-cpp/Simba.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/personagens-cpp/Tiao.cpp" "CMakeFiles/my_program.dir/personagens-cpp/Tiao.cpp.o" "gcc" "CMakeFiles/my_program.dir/personagens-cpp/Tiao.cpp.o.d"
+  "/mnt/c/Users/Mateus Bretz/Documents/UFMG_20241/PDS_2/simulador/simulador/principal/main.cpp" "CMakeFiles/my_program.dir/principal/main.cpp.o" "gcc" "CMakeFiles/my_program.dir/principal/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
